000fe4cd911e8388d1296a948f384e94fa6f4646
kiras <kiras@LAPTOP-FGI1U2OR>
2024-03-30 16:33:24 +0400

    Fixed generator: - support primitives - support the classes above primitives - support arrays (not primitives)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,17 @@ class Phone {
     long ph;
     char ch;
 }
+enum Sex {
+    Male,
+    Female
+}
 class Person {
+    //Sex sex;
     int a;
     protected double b;
-    int[] c;
+    Integer[] c;
     Phone ph;
+    String str;
      Person() {
         this.a = 5;
     }
@@ -18,17 +24,7 @@ class Person {
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
+    public static void main(String[] args) throws Exception {
         Person a = RandomObject.Generate(Person.class);
     }
 }
--- src/main/java/org/example/RandomObject.java
+++ src/main/java/org/example/RandomObject.java
@@ -2,35 +2,67 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 public class RandomObject<T> {
-    static long seed = System.nanoTime();
+    private static long seed = System.nanoTime();
     private static long Next() {
         seed ^= (seed << 21);
         seed ^= (seed >>> 35);
         seed ^= (seed << 4);
         return seed;
     }
-    public static <T> T Generate(Class<T> tClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        if (tClass.isPrimitive() || tClass.isEnum())
-            return tClass.cast(Next());
-        else if (tClass.isArray())
+    public static <T> T Generate(Class<T> tClass) throws Exception {
+        if (tClass.isArray())
             return tClass.cast(RandomArray(tClass.getComponentType()));
-        T obj = tClass.getDeclaredConstructor().newInstance();
+        T obj;
+        try {
+            obj = tClass.getDeclaredConstructor().newInstance();
+        }
+        catch (Exception ex) {
+            obj = switch (tClass.getName()) {
+                case "java.lang.Integer" -> obj = tClass.getDeclaredConstructor(int.class).newInstance((int) Next());
+                case "java.lang.Boolean" -> obj = tClass.getDeclaredConstructor(boolean.class).newInstance(Next() % 2 == 0);
+                case "java.lang.Double" -> obj = tClass.getDeclaredConstructor(double.class).newInstance((double) Next() / (double) Next());
+                case "java.lang.Long" -> obj = tClass.getDeclaredConstructor(long.class).newInstance(Next());
+                case "java.lang.Short" -> obj = tClass.getDeclaredConstructor(short.class).newInstance((short) Next());
+                case "java.lang.Char" -> obj = tClass.getDeclaredConstructor(char.class).newInstance((char) Next());
+                case "java.lang.Byte" -> obj = tClass.getDeclaredConstructor(byte.class).newInstance((byte) Next());
+                case "java.lang.Float" -> obj = tClass.getDeclaredConstructor(float.class).newInstance((float) Next() / (float) Next());
+                default -> throw new Exception("Object or component must have constructor without parameters\n" + ex.getMessage());
+            };
+            return obj;
+        }
         Field[] fields = tClass.getDeclaredFields();
+
         for (Field field : fields)
-            if (field.get(obj) == null)
-                field.set(obj, Generate(field.getType()));
+            if (!Modifier.isStatic(field.getModifiers())) {
+                field.setAccessible(true);
+                if (field.getType().isPrimitive()) {
+                    switch (field.getType().getName()) {
+                        case "int" -> field.set(obj, (int) Next());
+                        case "boolean" -> field.set(obj, Next() % 2 == 0);
+                        case "double" -> field.set(obj, (double) Next() / (double) Next());
+                        case "short" -> field.set(obj, (short) Next());
+                        case "byte" -> field.set(obj, (byte) Next());
+                        case "float" -> field.set(obj, (float) Next() / (float) Next());
+                        case "char" -> field.set(obj, (char) Next());
+                        default -> field.set(obj, Next());
+                    }
+                } else field.set(obj, Generate(field.getType()));
+            }
         return obj;
     }
-//    private static <P> P Random(Class<P> pClass) {
-//        return pClass.cast(Next());
-//    }
-    private static <T> T[] RandomArray(Class<T> tClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    private static <T> T[] RandomArray(Class<T> tClass) throws Exception {
         T[] arr = (T[])Array.newInstance(tClass, Math.abs((short)Next()));
         for (int i = 0; i < arr.length; ++i)
             arr[i] = Generate(tClass);
         return arr;
+    }
+    public static void SetSeed(long newSeed) {
+        seed = newSeed;
+    }
+    public static long GetSeed() {
+        return seed;
     }
 }
